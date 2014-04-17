cfab69db0b0e57e8bd7d6b8ad46ea60fe4fed197
Dustin Oprea <myselfasunder@gmail.com>
2014-04-17 13:23:46 -0400

    Added logging and Mac compatibility.

--- dev/archive.py
+++ dev/archive.py
@@ -3,12 +3,33 @@
 import sys
 sys.path.insert(0, '..')
 
+import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+os.environ['DYLD_LIBRARY_PATH'] = '/usr/local/Cellar/libarchive/3.1.2/lib'
+
 import libarchive
 
-#with libarchive.reader('test.7z') as reader:
-#    for e in reader:
-#        print("> %s" % (e))
+with libarchive.reader('test.7z') as reader:
+    for e in reader:
+        print("> %s" % (e))
 
-for entry in libarchive.pour('test.7z'):
-    print("Wrote: %s" % (entry))
+#for entry in libarchive.pour('test.7z'):
+#    print("Wrote: %s" % (entry))
 
--- libarchive/library.py
+++ libarchive/library.py
@@ -1,9 +1,15 @@
 import ctypes
 import ctypes.util
+import logging
+
+_logger = logging.getLogger(__name__)
 
 _LIB_FILEPATH = ctypes.util.find_library('libarchive')
 if _LIB_FILEPATH is None:
     _LIB_FILEPATH = 'libarchive.so'
+    _logger.debug("Could not use find_library to find library. Defaulting to "
+                  "'%s'." % (_LIB_FILEPATH))
+else:
+    _logger.debug("find_library rendered '%s'." % (_LIB_FILEPATH))
 
 libarchive = ctypes.cdll.LoadLibrary(_LIB_FILEPATH)
-

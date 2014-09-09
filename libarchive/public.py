1054a149cfadbff0ee2fc0cf8d84dfe32d9b3b93
Dustin Oprea <myselfasunder@gmail.com>
2014-09-08 23:40:50 -0400

    Bugfix.

--- libarchive/public.py
+++ libarchive/public.py
@@ -3,4 +3,4 @@ from libarchive.adapters.archive_read import \
     memory_enumerator, memory_reader, memory_pour
 
 from libarchive.adapters.archive_write import \
-    create_file, create_generic
+    create_file_from_files, create_stream_from_files, create_generic

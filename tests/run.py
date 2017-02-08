53617f7696dc502a4409dff90827c922181dffcb
Dustin Oprea <myselfasunder@gmail.com>
2017-02-08 13:40:39 -0500

    Removed environment-specific test logic.

--- tests/run.py
+++ tests/run.py
@@ -3,11 +3,6 @@
 import sys
 sys.path.insert(0, '..')
 
-import os
 import nose
-
-mac_library_path = '/Users/dustin/build/libarchive/build/libarchive'
-if os.path.exists(mac_library_path) is True:
-    os.environ['DYLD_LIBRARY_PATH'] = mac_library_path
 
 nose.run()
